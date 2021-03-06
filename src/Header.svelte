59f66cd5e9395af571bce771cea872a8a386e7a8
Christopher van der Bergh <cvanderbergh@gmail.com>
2021-03-06 14:25:57 +0100

    first commit

---
+++ src/Header.svelte
@@ -0,0 +1,12 @@
+<style>
+header {
+    background-color: #dae1f2;
+    padding: 2rem;
+}
+h1 {
+    font-family: Helvetica, sans-serif
+}
+</style>
+<header>
+    <h1>ISDGreen</h1>
+</header>
---
+++ src/Menu.svelte
@@ -0,0 +1,23 @@
+<script>
+    import {link} from 'svelte-spa-router'
+</script>
+<nav>
+    <a href="/" use:link>What is Climate Change?</a>
+    <a href="/cause" use:link>Is ISD Causing it?</a>
+    <a href="/solution" use:link>What can ISD do?</a>
+</nav>
+<style>
+    nav {
+        display: flex;
+        justify-content: space-evenly;
+        padding: 0.5rem;
+        background-color: black;
+    }
+    a {
+        font-family: 'Source Sans Pro', sans-serif;
+        font-size: 2rem;
+        font-weight: 600;
+        color: white;
+        text-decoration: none;
+    }
+</style>
